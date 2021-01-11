82b148cf09d498bb1df4b37e090c38dfd1091f87
RaisAryaguna <raisazaria30@gmail.com>
2021-01-11 15:14:24 +0700

    perubahan 11

--- Src/Screen/Penjemputan/Penjemputan.js
+++ Src/Screen/Penjemputan/Penjemputan.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, Image, TouchableOpacity} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 export class Penjemputan extends Component {
   render() {
     return (
       <View>
-        <Text> Penjemputan </Text>
+        <View style={{alignItems: 'center', margin: 10}}>
+          <Text style={{fontSize: 25}}> Penyetoran Sampah </Text>
+        </View>
+        <MapView
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation
+          showsMyLocationButton
+          provider={PROVIDER_GOOGLE}
+          style={{height: '100%'}}></MapView>
       </View>
     );
   }
